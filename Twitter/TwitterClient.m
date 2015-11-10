cf8574fef56f7f3c63c1e9caeda98f4d6288abae
Robin Wu <rwu@groupon.com>
2015-11-09 21:54:50 -0800

    update README and video walkthrough

--- Twitter/TwitterClient.m
+++ Twitter/TwitterClient.m
@@ -90,7 +90,7 @@ NSString * const kTwitterBaseUrl = @"https://api.twitter.com";
 - (void) homeTimelineWithParams: (NSDictionary *)params completion: (void (^)(NSArray *tweets, NSError *error)) completion {
     [self GET:@"1.1/statuses/home_timeline.json" parameters:params success:^(AFHTTPRequestOperation *operation, id responseObject) {
 
-        NSLog(@"Got tweets: %@", responseObject);
+        NSLog(@"Got tweets");
 
         NSArray *tweets = [Tweet tweetsWithArray:responseObject];
         completion(tweets, nil);
